69d98011ef5b17e14525a5ee8662e2b1bedab976
CodeSandbox <support@codesandbox.io>
2023-06-27 14:44:24 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="container mx-auto">
+      <header className="w-full text-white bg-gradient-to-r from-sky-500 to-blue-500">
+        <div className="2xl:mx-96 xl:mx-72 lg:mx-48 md:mx-12 mx-0">
+          <nav className="flex items-center justify-center py-6 uppercase">
+            <a href="#" className="px-4 hover:text-gray-300">
+              Blog
+            </a>
+            <a href="#" className="px-4 hover:text-gray-300">
+              Sobre
+            </a>
+            <a href="#" className="px-4 hover:text-gray-200">
+              Descrição
+            </a>
+          </nav>
+          <div className="flex flex-col sm:flex-row items-center p-4">
+            <div className="sm:w-56 w-48 shrink sm:shrink-0 pb-4 md:pb-0">
+              <img
+                src="https://i.pinimg.com/originals/fa/0c/05/fa0c05778206cb2b2dddf89267b7a31c.jpg"
+                className="w-full rounded-full"
+              />
+            </div>
+            <div className="ml-0 sm:ml-6">
+              <h1 className="text-4xl font-medium mb-2 text-center sm:text-left font-sans">
+                Seu nome
+              </h1>
+              <p>
+                Coloque aqui sua biografia de forma resumida. Coloque aqui sua
+                biografia de forma resumida. Coloque aqui sua biografia de forma
+                resumida. Coloque aqui sua biografia de forma resumida. Coloque
+                aqui sua biografia de forma resumida. Coloque aqui sua biografia
+                de forma resumida. Coloque aqui sua biografia de forma resumida.
+                Coloque aqui sua biografia de forma resumida. Coloque aqui sua
+                biografia de forma resumida
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="w-full p-4 pt-8">
+        <div className="2xl:mx-96 xl:mx-72 lg:mx-48 md:mx-12 mx-0">
+          <h2 className="text-xl font-medium font-sans mb-2">
+            Últimas do blog
+          </h2>
+          <article className="my-4">
+            <a href="#" className="font-medium text-sky-900 hover:text-sky-600">
+              5 dicas para sua carreira profissional
+            </a>
+            <p className="italic">
+              Confira algumas dicas que podem ajudar a alavancar sua carreira
+              como desenvolvedor front-end
+            </p>
+          </article>
+          <article className="my-4">
+            <a href="#" className="font-medium text-sky-900 hover:text-sky-600">
+              5 dicas para sua carreira profissional
+            </a>
+            <p className="italic">
+              Confira algumas dicas que podem ajudar a alavancar sua carreira
+              como desenvolvedor front-end
+            </p>
+          </article>
+          <article className="my-4 mb-6">
+            <a href="#" className="font-medium text-sky-900 hover:text-sky-600">
+              5 dicas para sua carreira profissional
+            </a>
+            <p className="italic">
+              Confira algumas dicas que podem ajudar a alavancar sua carreira
+              como desenvolvedor front-end
+            </p>
+          </article>
+          <a href="#" className="font-medium text-sky-900 hover:text-sky-600">
+            Ver tudo
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
